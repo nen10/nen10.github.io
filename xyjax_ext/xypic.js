33c24dc4368be8135b14d23946c40db8adb5e2c0
nen10 <aqua5x4@i.softbank.jp>
2020-01-07 07:12:02 +0900

    install xyjax

--- xyjax_ext/xypic.js
+++ xyjax_ext/xypic.js
@@ -16769,4 +16769,4 @@ MathJax.Hub.Register.StartupHook("SVG Xy-pic Require",function () {
   MathJax.Hub.Startup.signal.Post("SVG Xy-pic Ready");
 });
 
-MathJax.Ajax.loadComplete("http://nen10.github.io/xyjax_ext/xypic.js");
+MathJax.Ajax.loadComplete("https://nen10.github.io/xyjax_ext/xypic.js");
